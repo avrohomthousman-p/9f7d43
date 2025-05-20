aa6b5bf3e301e65feed0502c6cad9be8b3e11d95
avrohomthousman-p <avrohomthousman@gmail.com>
2025-05-20 09:41:58 -0400

    feat: setup function to query API

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 
 // Create a server
-const server = http.createServer((req, res) => {
-    if (req.url.match(/\/api\/v1\/[^/]+\/actions\/blueprints\/[^/]+\/graph/)  && req.method === 'GET') {
+const server = http.createServer(async (req, res) => {
+    if (req.url.match(/\/api\/v1\/[^/]+\/actions\/blueprints\/[^/]+\/[^/]+\/graph/)  && req.method === 'GET') {
         const filePath = path.join(__dirname, 'graph.json');
 
         // Read the graph.json file
@@ -23,6 +23,31 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({error: 'Resource not found!'}));
     }
 });
+
+
+
+/**
+ * Executes a query to the API and returns the results
+ * @param req the request recieved by our server
+ * @returns {Promise<any>} results of the API query
+ */
+async function queryAPI(req){
+    //TODO: get API key so this can actually work
+    const groups = req.url.match(/\/api\/v1\/([^/]+)\/actions\/blueprints\/([^/]+)\/([^/]+)\/graph/);
+    const url = `https://api.avantos-dev.io/api/v1/${ groups[1] }/actions/blueprints/${ groups[2] }/${ groups[3] }/graph`;
+
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json, application/problem+json',
+        }
+    });
+    console.log(response);
+    return await response.json();
+}
+
+
 
 // Start the server on port 3000
 server.listen(3000, () => {
